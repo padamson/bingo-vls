efbb2dfd9cf013ca6ad91361ac8bcaf6fbcc64ef
Paul Adamson <paul.adamson@nrl.navy.mil>
2020-11-12 23:08:23 -0500

    initialize repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# NRL Bingo
+
+This is an adaptation of another Bingo game found on github.
+
+Replace the words in the `words.js` file with your own ones. Keep the first and last line!
+
+Like this:
+```
+const words = `
+
+Here comes your own bs
+Each one goes in a separate line
+
+`;
+```
+
+Open the `index.html` in a browser. Every time you refresh the page it will generate a new Bingo card.  
+Print it or play it on your laptop. Have fun! :wink:
---
+++ words.js
@@ -0,0 +1,33 @@
+const words = `
+
+
+[Drowning robot voice]
+[Dog barking]
+[Talking while muted]
+[Frozen video/audio]
+[Talk on top of someone] 
+[Talk on top of someone multiple times]
+[Kicked out]
+Game changer
+Bandwidth
+Transparency
+Strategic
+Core competency(ies)
+Out of the box
+Bottom line
+Revisit
+Take that off-line
+Out of the loop
+Benchmark
+Value-added
+Thought leader
+Proactive
+Win-win
+Win-win-win
+Fast track
+At the end of the day
+Touch base
+Leverage
+
+
+`;
